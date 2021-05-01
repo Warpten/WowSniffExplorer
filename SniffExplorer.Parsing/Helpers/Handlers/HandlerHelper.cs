23b2c08d24d0cfc529e6854398d8a2c7efb921c6
Warpten <vertozor@gmail.com>
2021-05-02 00:10:24 +0200

    Add the ability for packet handlers to return disposable objects that will be collected when parsing is completed.

--- SniffExplorer.Parsing/Helpers/Handlers/HandlerHelper.cs
+++ SniffExplorer.Parsing/Helpers/Handlers/HandlerHelper.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Reflection;
-using System.Threading.Tasks;
 using SniffExplorer.Parsing.Attributes;
 using SniffExplorer.Parsing.Engine;
 using SniffExplorer.Parsing.Types;
@@ -12,75 +11,101 @@ using SniffExplorer.Shared.Enums;
 namespace SniffExplorer.Parsing.Helpers.Handlers
 {
     /// <summary>
-    /// This interface is in charge of dispatching packet processing to the appropriate methods.
+    /// This object is in charge of dispatching packet processing to the appropriately declared methods.
+    ///
+    /// Handlers may have one of the following signatures:
+    /// <ul>
+    ///     <li>IDisposable[] ProcessPacket(ParsingContext context, Packet packet);</li>
+    ///     <li>void ProcessPacket(ParsingContext context, Packet packet);</li>
+    /// </ul>
+    /// 
+    /// Any other return type will cause a runtime exception to occur.
     /// </summary>
     public class HandlerHelper
     {
-        private class Store
+        // Stores instances of 
+        private readonly Dictionary<Type, object> _instances = new();
+        private readonly Dictionary<(PacketDirection Direction, Opcode Opcode), Func<ParsingContext, Packet, IDisposable[]>> _dataStore = new();
+        
+        /// <summary>
+        /// Given a <see cref="MethodInfo"/>, constructs a delegate that can be called for packet processing.
+        /// </summary>
+        /// <param name="methodInfo"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        private Func<ParsingContext, Packet, IDisposable[]> CreateDelegate(MethodInfo methodInfo, ParsingContext context)
         {
-            private readonly Dictionary<Type, object> _instances = new();
-            private readonly Dictionary<(PacketDirection Direction, Opcode Opcode), Action<ParsingContext, Packet>> _dataStore = new();
-
-            internal void Insert(ParsingContext context, PacketDirection direction, Opcode value, MethodInfo methodInfo)
+            var contextParameter = Expression.Parameter(typeof(ParsingContext), "context");
+            var packetParameter = Expression.Parameter(typeof(Packet), "packet");
+            
+            if (methodInfo.IsStatic)
             {
-                var contextParameter = Expression.Parameter(typeof(ParsingContext), "context");
-                var packetParameter = Expression.Parameter(typeof(Packet), "packet");
-
-                if (methodInfo.IsStatic)
+                var methodCallExpr = Expression.Call(methodInfo, contextParameter, packetParameter);
+                
+                if (methodInfo.ReturnType == typeof(void))
                 {
-                    var methodCallExpr = Expression.Call(methodInfo, contextParameter, packetParameter);
-                    var lambda = Expression
-                        .Lambda<Action<ParsingContext, Packet>>(methodCallExpr,
-                            contextParameter, packetParameter);
+                    var lambdaExpression = Expression.Lambda<Action<ParsingContext, Packet>>(methodCallExpr, contextParameter, packetParameter);
+                    var lambda = lambdaExpression.Compile();
 
-                    Insert(direction, value, lambda.Compile());
+                    return (parsingContext, packet) => {
+                        lambda.Invoke(parsingContext, packet);
+                        return new IDisposable[0];
+                    };
                 }
                 else
                 {
-                    var declaringType = methodInfo.DeclaringType!;
-                    // Can't use declaringType for the parameter type due to runtime checks in expressions.
-                    var instanceParameter = Expression.Parameter(typeof(object), "instance");
+                    if (methodInfo.ReturnType != typeof(IDisposable[]))
+                        throw new InvalidOperationException($"Method {methodInfo.Name} (declared in {methodInfo.DeclaringType!.FullName}) does not have a proper signature. Packet will be skipped.");
 
-                    var methodCallExpr =
-                        Expression.Call(Expression.Convert(instanceParameter, declaringType), methodInfo, contextParameter, packetParameter);
-                    var lambda = Expression.Lambda<Action<object, ParsingContext, Packet>>(methodCallExpr, instanceParameter, contextParameter, packetParameter);
-
-                    var compiledLambda = lambda.Compile();
-
-                    if (!_instances.TryGetValue(methodInfo.DeclaringType!, out var instance))
-                    {
-                        // If a constructor taking ParsingContext is found, use it. Else, default construct.
-                        var constructorInfo = declaringType.GetConstructor(new [] { typeof(ParsingContext) });
-                        instance = constructorInfo != null
-                            ? Activator.CreateInstance(declaringType, context) 
-                            : Activator.CreateInstance(declaringType);
-
-                        _instances[methodInfo.DeclaringType!] = instance
-                            ?? throw new InvalidOperationException($"Unable to create an instance of {declaringType.FullName}.");
-                    }
-
-                    Insert(direction, value, (ctx, packet) => compiledLambda.Invoke(instance!, ctx, packet));
+                    var lambdaExpression = Expression.Lambda<Func<ParsingContext, Packet, IDisposable[]>>(methodCallExpr, contextParameter, packetParameter);
+                    return lambdaExpression.Compile();
                 }
             }
-
-            public bool CanProcess(PacketDirection direction, Opcode opcode)
-                => _dataStore.ContainsKey((direction, opcode));
-
-            private void Insert(PacketDirection direction, Opcode opcode, Action<ParsingContext, Packet> fn)
-                => _dataStore[(direction, opcode)] = fn;
-
-            public void InvokeMethod(ParsingContext context, Packet packet)
+            else
             {
-                if (_dataStore.TryGetValue((packet.Direction, packet.Opcode), out var @delegate))
-                    @delegate.Invoke(context, packet);
+                var declaringType = methodInfo.DeclaringType!;
+                // Can't use declaringType for the parameter type due to runtime checks in expressions.
+                // (Func<object, ...> would not match the actual type)
+                var instanceParameter = Expression.Parameter(typeof(object), "instance");
+
+                // Retrieve the instanciated type.
+                if (!_instances.TryGetValue(methodInfo.DeclaringType!, out var instance))
+                {
+                    // If a constructor taking ParsingContext is found, use it. Else, default construct.
+                    var constructorInfo = declaringType.GetConstructor(new [] { typeof(ParsingContext) });
+                    instance = constructorInfo != null
+                        ? Activator.CreateInstance(declaringType, context) 
+                        : Activator.CreateInstance(declaringType);
+
+                    _instances[methodInfo.DeclaringType!] = instance ?? throw new InvalidOperationException($"Unable to create an instance of {declaringType.FullName}.");
+                }
+
+                var methodCallExpr = Expression.Call(Expression.Convert(instanceParameter, declaringType), methodInfo, contextParameter, packetParameter);
+
+                if (methodInfo.ReturnType == typeof(void))
+                {
+                    var lambdaExpression = Expression.Lambda<Action<object, ParsingContext, Packet>>(methodCallExpr, instanceParameter, contextParameter, packetParameter);
+                    var lambda = lambdaExpression.Compile();
+
+                    return (parsingContext, packet) =>
+                    {
+                        lambda.Invoke(instance, parsingContext, packet);
+                        return new IDisposable[0];
+                    };
+                }
+                else
+                {
+                    if (methodInfo.ReturnType != typeof(IDisposable[]))
+                        throw new InvalidOperationException($"Method {methodInfo.Name} (declared in {methodInfo.DeclaringType!.FullName}) does not have a proper signature. Packet will be skipped.");
+
+                    var lambdaExpression = Expression.Lambda<Func<object, ParsingContext, Packet, IDisposable[]>>(methodCallExpr, instanceParameter, contextParameter, packetParameter);
+                    var lambda = lambdaExpression.Compile();
+
+                    return (parsingContext, packet) => lambda.Invoke(instance, parsingContext, packet);
+                }
             }
-
-            public async Task InvokeMethodAsync(ParsingContext context, Packet packet)
-                => await Task.Run(() => InvokeMethod(context, packet)).ConfigureAwait(false);
         }
-
-        private readonly Store _packetHandlers = new();
-
+        
         private ParsingContext Context { get; }
 
         public HandlerHelper(ParsingContext context, Type[] assemblyTypes)
@@ -92,14 +117,16 @@ namespace SniffExplorer.Parsing.Helpers.Handlers
                 foreach (var methodInfo in type.GetMethods(BindingFlags.Public | BindingFlags.Static | BindingFlags.Instance))
                 {
                     var attributes = methodInfo.GetCustomAttributes<ParserAttribute>();
+
+                    var @delegate = CreateDelegate(methodInfo, context);
                     foreach (var attribute in attributes)
-                        _packetHandlers.Insert(context, attribute.Direction, attribute.Opcode, methodInfo);
+                        _dataStore.Add((attribute.Direction, attribute.Opcode), @delegate);
                 }
             }
         }
 
         public bool CanProcess(PacketDirection direction, Opcode opcode)
-            => _packetHandlers.CanProcess(direction, opcode);
+            => _dataStore.ContainsKey((direction, opcode));
 
         /// <summary>
         /// Processes a packet.
@@ -108,20 +135,12 @@ namespace SniffExplorer.Parsing.Helpers.Handlers
         /// <returns></returns>
         public bool Process(Packet packet)
         {
-            _packetHandlers.InvokeMethod(Context, packet);
-
-            return true;
-            // return packet.FinalizeRead();
-        }
-
-        /// <summary>
-        /// Asynchronously processes a packet.
-        /// </summary>
-        /// <param name="packet">The packet to process.</param>
-        /// <returns></returns>
-        public async Task<bool> ProcessAsync(Packet packet)
-        {
-            await _packetHandlers.InvokeMethodAsync(Context, packet);
+            if (_dataStore.TryGetValue((packet.Direction, packet.Opcode), out var @delegate))
+            {
+                var resources = @delegate.Invoke(Context, packet);
+                foreach (var resource in resources)
+                    Context.RegisterResource(resource);
+            }
 
             return true;
             // return packet.FinalizeRead();
